77f25c2f0ee1055db0f28046574368dd245631d1
Brandon Weaver <keystonelemur@gmail.com>
2024-05-15 14:45:42 +0900

    More pattern match examples and reading

--- 02_pattern_match_intro.rb
+++ 02_pattern_match_intro.rb
@@ -8,3 +8,10 @@ n
 # Find pattern
 other_array = [1, 2, 'a', 3, 'b', 4, 5]
 other_array in [*, 'a', *]
+# => true
+
+# Expression pattern
+[1, 2, 3] in [Integer => n, ^(n + 1), ^(n + 2)]
+# => true
+
+# Further reading: https://dev.to/baweaver/definitive-pattern-matching-array-like-structures-1298
